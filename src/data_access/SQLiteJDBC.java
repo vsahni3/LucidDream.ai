a914b81a5ee61e107b3ae3d2aec05c54e8431a77
Varun Sahni <varunsahni3504@gmail.com>
2023-11-06 12:59:58 -0500

    finish table creation and set up of SQL DB

---
+++ src/data_access/SQLiteJDBC.java
@@ -0,0 +1,70 @@
+package data_access;
+import java.sql.*;
+
+public class SQLiteJDBC {
+    private Connection c;
+    public SQLiteJDBC() {
+
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            this.c = DriverManager.getConnection("jdbc:sqlite:test.db");
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+
+    public Connection getConnection() {
+        return this.c;
+    }
+    public void createUserTable() {
+        Statement stmt = null;
+        try {
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS USER " +
+                    "(USERNAME TEXT PRIMARY KEY     NOT NULL," +
+                    " PASSWORD           TEXT    NOT NULL, )";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            System.out.println("Table created successfully");
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+    }
+
+    public void createBookTable() {
+        Statement stmt = null;
+        try {
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS BOOK " +
+                    "(TITLE TEXT PRIMARY KEY     NOT NULL," +
+                    "StoryText           TEXT    NOT NULL, " +
+                    "FOREIGN KEY (UserID) REFERENCES User(ID)";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            System.out.println("Table created successfully");
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+    }
+
+    public void createPageTable() {
+        Statement stmt = null;
+        try {
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE IF NOT EXISTS PAGE " +
+                    "(ID INTEGER PRIMARY KEY     NOT NULL," +
+                    "TextContent           TEXT    NOT NULL, " +
+                    "PageNumber     INTEGER NOT NULL, " +
+                    "FOREIGN KEY (BookID) REFERENCES BOOK(TITLE)";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            System.out.println("Table created successfully");
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+    }
+}
+
---
+++ src/data_access/SqlBookDataAccessObject.java
@@ -0,0 +1,106 @@
+package data_access;
+import java.sql.*;
+
+import data_access.SQLiteJDBC;
+import entity.User;
+import entity.UserFactory;
+import use_case.login.LoginUserDataAccessInterface;
+import use_case.signup.SignupUserDataAccessInterface;
+
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class SqlBookDataAccessObject implements SignupUserDataAccessInterface, LoginUserDataAccessInterface {
+
+    private final Connection c;
+
+    private final Map<String, Integer> headers = new LinkedHashMap<>();
+
+    private final Map<String, Book> books = new HashMap<>();
+
+    private BookFactory bookFactory;
+
+    public SqlUserDataAccessObject(SQLiteJDBC connector, BookFactory bookFactory) throws IOException {
+        this.bookFactory = bookFactory;
+        this.c = connector.getConnection();
+        connector.createBookTable();
+
+        loadData(books);
+
+    }
+
+    public void loadData(Map<String, User> map) {
+        String sql = "SELECT USERNAME, PASSWORD FROM BOOK";
+
+        try (Statement stmt = c.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                String title = rs.getString("TITLE");
+                String storyText = rs.getString("StoryText");
+                Book book = bookFactory.create(title, storyText);
+                map.put(title, book);
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+
+    }
+
+    @Override
+    public void save(Book book) {
+        books.put(book.getTitle(), book);
+        this.save();
+    }
+
+    @Override
+    public Book get(String title) {
+        return books.get(title);
+    }
+
+    private void save() {
+        Map<String, Book> tempMap = new HashMap<>();
+        loadData(tempMap);
+
+        for (String title : books.keySet()) {
+            Book book = books.get(title);
+            if (tempMap.containsKey(title)) {
+                String sql = "UPDATE BOOK SET StoryText = ? WHERE TITLE = ?";
+                try (PreparedStatement pstmt = c.prepareStatement(sql)) {
+                    pstmt.setString(1, book.getStoryText()); // Set the password parameter
+                    pstmt.executeUpdate(); // Execute the insert statement
+                } catch (SQLException e) {
+                    System.err.println(e.getMessage());
+                }
+            } else {
+                String sql = "INSERT INTO USER (USERNAME, PASSWORD) VALUES (?, ?)";
+                try (PreparedStatement pstmt = c.prepareStatement(sql)) {
+                    pstmt.setString(1, username); // Set the username parameter
+                    pstmt.setString(2, user.getPassword()); // Set the password parameter
+                    pstmt.executeUpdate(); // Execute the insert statement
+
+                } catch (SQLException e) {
+                    System.err.println(e.getMessage());
+                }
+
+            }
+        }
+
+    }
+
+
+    /**
+     * Return whether a user exists with username identifier.
+     * @param identifier the username to check.
+     * @return whether a user exists with username identifier
+     */
+    @Override
+    public boolean existsByName(String identifier) {
+        return accounts.containsKey(identifier);
+    }
+
+
+}
