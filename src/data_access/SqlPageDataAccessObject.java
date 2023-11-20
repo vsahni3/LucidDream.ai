47fbe8ad2d4261fd1091c300d0cd6d801e8dfbca
Varun Sahni <varunsahni3504@gmail.com>
2023-11-20 14:46:26 -0500

    add logic in dao to convert between byte[] and BLOB data type

--- src/data_access/SqlPageDataAccessObject.java
+++ src/data_access/SqlPageDataAccessObject.java
@@ -12,7 +12,18 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-
+/**
+ * The SqlPageDataAccessObject class is designed for handling all database operations related to Page entities.
+ * It implements the SignupUserDataAccessInterface and LoginUserDataAccessInterface, providing specific functionalities
+ * for page-related operations during user signup and login processes.
+ *
+ * This class manages a collection of Page objects, offering methods to load, retrieve, and save page data. It uses
+ * a SQL database connection provided by a SQLiteJDBC connector and a PageFactory for creating Page instances.
+ *
+ * The key functionalities include retrieving pages associated with a specific book title, and saving individual pages
+ * or a collection of pages in the database. It ensures efficient and effective management of page data in alignment
+ * with the system's requirements for content management and presentation.
+ */
 public class SqlPageDataAccessObject implements SignupUserDataAccessInterface, LoginUserDataAccessInterface {
 
     private final Connection c;
@@ -38,7 +49,7 @@ public class SqlPageDataAccessObject implements SignupUserDataAccessInterface, L
     }
 
     private void loadData(Map<Integer, Page> map) {
-        String sql = "SELECT pageContents, pageNumber FROM BOOK";
+        String sql = "SELECT pageContents, pageNumber, image FROM PAGE";
 
         try (Statement stmt = c.createStatement();
              ResultSet rs = stmt.executeQuery(sql)) {
@@ -47,7 +58,10 @@ public class SqlPageDataAccessObject implements SignupUserDataAccessInterface, L
                 Integer id = rs.getInt("ID");
                 String pageContents = rs.getString("pageContents");
                 String pageNumber = rs.getString("pageNumber");
-                Page page = pageFactory.create(id, pageContents, pageNumber);
+                Blob imageBlob = rs.getBlob("image");
+                byte[] image = imageBlob.getBytes(1, (int) imageBlob.length());
+                imageBlob.free(); // Free the blob resource
+                Page page = pageFactory.create(id, pageContents, pageNumber, image);
                 map.put(id, page);
             }
         } catch (SQLException e) {
@@ -96,7 +110,7 @@ public class SqlPageDataAccessObject implements SignupUserDataAccessInterface, L
             try (PreparedStatement pstmt = c.prepareStatement(sql)) {
                 pstmt.setString(1, page.getPageContents()); // Set the password parameter
                 pstmt.setString(2, page.getPageNumber()); // Set the userna,e parameter
-                pstmt.setString(3, page.getImage());
+                pstmt.setBytes(3, page.getImage());
                 pstmt.setString(4, title);
                 pstmt.setString(5, book.getID());
                 pstmt.executeUpdate(); // Execute the insert statement
@@ -109,7 +123,7 @@ public class SqlPageDataAccessObject implements SignupUserDataAccessInterface, L
             try (PreparedStatement pstmt = c.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
                 pstmt.setString(1, page.getPageContents()); // Set the username parameter
                 pstmt.setString(2, page.getPageNumber()); // Set the password parameter
-                pstmt.setString(3, page.getImage());
+                pstmt.setBytes(3, page.getImage());
                 pstmt.setString(4, title);
                 pstmt.executeUpdate(); // Execute the insert statement
                 ResultSet generatedKeys = pstmt.getGeneratedKeys();
