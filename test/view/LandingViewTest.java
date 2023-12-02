b2f80cd1bfde47e58aeef9f9a0e2fd3a23fdc5e4
cho4 <eugenecho0425@gmail.com>
2023-12-02 16:01:45 -0500

    added tests for Landing, Login and Signup views

---
+++ test/view/LandingViewTest.java
@@ -0,0 +1,201 @@
+package view;
+
+import app.LoginUseCaseFactory;
+import app.SignupUseCaseFactory;
+import data_access.InMemoryUserDataAccessObject;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.logged_in.LoggedInViewModel;
+import interface_adapter.login.LoginViewModel;
+import interface_adapter.signup.SignupViewModel;
+import org.junit.jupiter.api.Test;
+import use_case.login.LoginUserDataAccessInterface;
+import use_case.signup.SignupUserDataAccessInterface;
+import javax.swing.*;
+import java.awt.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class LandingViewTest {
+
+    @Test
+    public void testLandingViewDisplays() {
+
+        JFrame app = new JFrame();
+        CardLayout cardLayout = new CardLayout();
+        JPanel views = new JPanel(cardLayout);
+        app.add(views);
+
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        SignupViewModel signupViewModel = new SignupViewModel();
+        LoginViewModel loginViewModel = new LoginViewModel();
+
+        new ViewManager(views, cardLayout, viewManagerModel);
+        LandingView landingView = new LandingView(signupViewModel, loginViewModel, viewManagerModel);
+
+        views.add(landingView, landingView.viewName);
+        viewManagerModel.setActiveView(landingView.viewName);
+        viewManagerModel.firePropertyChanged();
+
+        app.pack();
+        app.setVisible(true);
+        assertEquals(viewManagerModel.getActiveView(), "landing page");
+    };
+
+
+    public JButton getLoginButton() {
+        JFrame app = null;
+
+        Window[] windows = Window.getWindows();
+        for (Window window : windows) {
+            if (window instanceof JFrame) {
+                app = (JFrame) window;
+            }
+        }
+
+        assertNotNull(app); // found the window?
+        Component root = app.getComponent(0);
+        Component cp = ((JRootPane) root).getContentPane();
+        JPanel jp = (JPanel) cp;
+        JPanel jp2 = (JPanel) jp.getComponent(0);
+        LandingView sv = (LandingView) jp2.getComponent(0);
+        JPanel buttons = (JPanel) sv.getComponent(4);
+        return (JButton) buttons.getComponent(0);
+    }
+
+    public JButton getSignupButton() {
+        JFrame app = null;
+
+        Window[] windows = Window.getWindows();
+        for (Window window : windows) {
+            if (window instanceof JFrame) {
+                app = (JFrame) window;
+            }
+        }
+
+        assertNotNull(app); // found the window?
+        Component root = app.getComponent(0);
+        Component cp = ((JRootPane) root).getContentPane();
+        JPanel jp = (JPanel) cp;
+        JPanel jp2 = (JPanel) jp.getComponent(0);
+        LandingView sv = (LandingView) jp2.getComponent(0);
+        JPanel buttons = (JPanel) sv.getComponent(4);
+        return (JButton) buttons.getComponent(2);
+    }
+
+
+    @Test
+    public void testLoginButtonPresent() {
+        JFrame app = new JFrame();
+        CardLayout cardLayout = new CardLayout();
+        JPanel views = new JPanel(cardLayout);
+        app.add(views);
+
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        SignupViewModel signupViewModel = new SignupViewModel();
+        LoginViewModel loginViewModel = new LoginViewModel();
+        new ViewManager(views, cardLayout, viewManagerModel);
+
+        LandingView landingView = new LandingView(signupViewModel, loginViewModel, viewManagerModel);
+        views.add(landingView, landingView.viewName);
+
+        app.pack();
+        app.setVisible(true);
+
+        JButton button = getLoginButton();
+        assert(button.getText().equals("Login"));
+    }
+
+
+    @Test
+    public void testSignupButtonPresent() {
+        JFrame app = new JFrame();
+        CardLayout cardLayout = new CardLayout();
+        JPanel views = new JPanel(cardLayout);
+        app.add(views);
+
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        SignupViewModel signupViewModel = new SignupViewModel();
+        LoginViewModel loginViewModel = new LoginViewModel();
+        new ViewManager(views, cardLayout, viewManagerModel);
+
+        LandingView landingView = new LandingView(signupViewModel, loginViewModel, viewManagerModel);
+        views.add(landingView, landingView.viewName);
+
+        app.pack();
+        app.setVisible(true);
+
+        JButton button = getSignupButton();
+        assert(button.getText().equals("Sign Up"));
+    }
+
+    @Test
+    public void testOpensLoginView() {
+        JFrame app = new JFrame();
+        CardLayout cardLayout = new CardLayout();
+        JPanel views = new JPanel(cardLayout);
+        app.add(views);
+
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        SignupViewModel signupViewModel = new SignupViewModel();
+        LoginViewModel loginViewModel = new LoginViewModel();
+        LoggedInViewModel loggedInViewModel = new LoggedInViewModel();
+        LoginUserDataAccessInterface loginUserDataAccessInterface = new InMemoryUserDataAccessObject();
+        new ViewManager(views, cardLayout, viewManagerModel);
+
+        LandingView landingView = new LandingView(signupViewModel, loginViewModel, viewManagerModel);
+        LoginView loginView = LoginUseCaseFactory.create(viewManagerModel, loginViewModel, loggedInViewModel, loginUserDataAccessInterface);
+
+        views.add(landingView, landingView.viewName);
+        views.add(loginView, loginView.viewName);
+
+        viewManagerModel.setActiveView(landingView.viewName);
+        viewManagerModel.firePropertyChanged();
+
+        app.pack();
+        app.setVisible(true);
+        JButton button = getLoginButton();
+        button.doClick();
+
+        assertEquals(viewManagerModel.getActiveView(), "log in");
+
+    }
+
+    @Test
+    public void testOpensSignupView() {
+        JFrame app = new JFrame();
+        CardLayout cardLayout = new CardLayout();
+        JPanel views = new JPanel(cardLayout);
+        app.add(views);
+
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        SignupViewModel signupViewModel = new SignupViewModel();
+        LoginViewModel loginViewModel = new LoginViewModel();
+        LoggedInViewModel loggedInViewModel = new LoggedInViewModel();
+        SignupUserDataAccessInterface signupUserDataAccessInterface = new InMemoryUserDataAccessObject();
+        new ViewManager(views, cardLayout, viewManagerModel);
+
+        LandingView landingView = new LandingView(signupViewModel, loginViewModel, viewManagerModel);
+        SignupView signupView = SignupUseCaseFactory.create(viewManagerModel, loggedInViewModel, signupViewModel,signupUserDataAccessInterface);
+
+        views.add(landingView, landingView.viewName);
+        views.add(signupView, signupView.viewName);
+
+        viewManagerModel.setActiveView(landingView.viewName);
+        viewManagerModel.firePropertyChanged();
+
+        app.pack();
+        app.setVisible(true);
+        JButton button = getSignupButton();
+        button.doClick();
+
+        assertEquals(viewManagerModel.getActiveView(), "sign up");
+
+    }
+}
+
+
+
+
+
+
+
---
+++ test/view/SignupViewTest.java
@@ -0,0 +1,251 @@
+package view;
+
+import app.SignupUseCaseFactory;
+import data_access.InMemoryUserDataAccessObject;
+import entity.CommonUserFactory;
+import entity.User;
+import entity.UserFactory;
+import interface_adapter.ViewManagerModel;
+import interface_adapter.generate_story.GenerateStoryController;
+import interface_adapter.logged_in.LoggedInViewModel;
+import interface_adapter.login.LoginState;
+import interface_adapter.login.LoginViewModel;
+import interface_adapter.signup.SignupController;
+import interface_adapter.signup.SignupPresenter;
+import interface_adapter.signup.SignupState;
+import interface_adapter.signup.SignupViewModel;
+import use_case.generate.GenerateInputBoundary;
+import use_case.login.LoginOutputBoundary;
+import use_case.login.LoginOutputData;
+import use_case.signup.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+import org.junit.jupiter.api.Test;
+
+import static java.lang.Thread.sleep;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SignupViewTest {
+
+    @Test
+    public void testSignupView() {
+
+        SignupInputBoundary sib = null;
+        SignupController signupController = new SignupController(sib);
+
+        SignupViewModel signupViewModel = new SignupViewModel();
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        SignupView signupView = new SignupView(signupController, signupViewModel, viewManagerModel);
+
+        JFrame app = new JFrame();
+        CardLayout cardLayout = new CardLayout();
+        JPanel views = new JPanel(cardLayout);
+        app.add(views);
+
+        new ViewManager(views, cardLayout, viewManagerModel);
+
+        views.add(signupView, signupView.viewName);
+
+        viewManagerModel.setActiveView(signupView.viewName);
+        viewManagerModel.firePropertyChanged();
+
+        app.pack();
+        app.setVisible(true);
+
+        JPanel panel = (JPanel) signupView.getComponent(3);
+
+        LabelTextPanel passwordLabelTextPanel = (LabelTextPanel) panel.getComponent(2);
+        JPasswordField pwdField = (JPasswordField) passwordLabelTextPanel.getComponent(2);
+
+        LabelTextPanel repeatPasswordLabelTextPanel = (LabelTextPanel) panel.getComponent(4);
+        JPasswordField repeatPwdField = (JPasswordField) repeatPasswordLabelTextPanel.getComponent(2);
+
+
+        KeyEvent pwdEvent = new KeyEvent(
+                pwdField,
+                KeyEvent.KEY_TYPED,
+                System.currentTimeMillis(),
+                0,
+                KeyEvent.VK_UNDEFINED,
+                'y');
+
+
+        KeyEvent rptPwdEvent = new KeyEvent(
+                repeatPwdField,
+                KeyEvent.KEY_TYPED,
+                System.currentTimeMillis(),
+                0,
+                KeyEvent.VK_UNDEFINED,
+                'y');
+
+
+        panel.dispatchEvent(pwdEvent);
+        panel.dispatchEvent(rptPwdEvent);
+
+
+        try {
+            sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        KeyEvent pwdEventRight = new KeyEvent(
+                pwdField,
+                KeyEvent.KEY_PRESSED,
+                System.currentTimeMillis(),
+                0,
+                KeyEvent.VK_RIGHT,
+                KeyEvent.CHAR_UNDEFINED
+        );
+
+
+        KeyEvent rptPwdEventRight = new KeyEvent(
+                repeatPwdField,
+                KeyEvent.KEY_PRESSED,
+                System.currentTimeMillis(),
+                0,
+                KeyEvent.VK_RIGHT,
+                KeyEvent.CHAR_UNDEFINED
+        );
+
+        panel.dispatchEvent(pwdEventRight);
+        panel.dispatchEvent(rptPwdEventRight);
+
+        try {
+            sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // type a second character
+        KeyEvent pwdEvent2 = new KeyEvent(
+                pwdField,
+                KeyEvent.KEY_TYPED,
+                System.currentTimeMillis(),
+                0,
+                KeyEvent.VK_UNDEFINED,
+                'z');
+
+        // type a second character
+        KeyEvent rptPwdEvent2 = new KeyEvent(
+                repeatPwdField,
+                KeyEvent.KEY_TYPED,
+                System.currentTimeMillis(),
+                0,
+                KeyEvent.VK_UNDEFINED,
+                'z');
+
+
+        panel.dispatchEvent(pwdEvent2);
+        panel.dispatchEvent(rptPwdEvent2);
+
+
+        // pause execution for 3 seconds
+        try {
+            sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        // assert that the values are as expected.
+        assertEquals("yz", new String(pwdField.getPassword()));
+        assertEquals("yz", signupViewModel.getState().getPassword());
+        assertEquals("yz", new String(repeatPwdField.getPassword()));
+        assertEquals("yz", signupViewModel.getState().getRepeatPassword());
+    }
+
+
+    @Test
+    public void testOpensLoggedInView() {
+
+        SignupUserDataAccessInterface userRepository = new InMemoryUserDataAccessObject();
+        UserFactory factory = new CommonUserFactory();
+
+        LoggedInViewModel loggedInViewModel = new LoggedInViewModel();
+        SignupViewModel signupViewModel = new SignupViewModel();
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+
+        SignupOutputBoundary signupPresenter = new SignupPresenter(viewManagerModel, signupViewModel, loggedInViewModel);
+
+        SignupInputBoundary sib = new SignupInteractor(userRepository, signupPresenter, factory);
+        SignupController signupController = new SignupController(sib);
+        SignupView signupView = new SignupView(signupController, signupViewModel, viewManagerModel);
+
+
+        GenerateInputBoundary gib = null;
+        GenerateStoryController generateStoryController = new GenerateStoryController(gib);
+        LoggedInView loggedInView = new LoggedInView(loggedInViewModel, viewManagerModel, generateStoryController);
+
+        JFrame app = new JFrame();
+        CardLayout cardLayout = new CardLayout();
+        JPanel views = new JPanel(cardLayout);
+        app.add(views);
+
+        new ViewManager(views, cardLayout, viewManagerModel);
+
+        views.add(signupView, signupView.viewName);
+        views.add(loggedInView, loggedInView.viewName);
+
+        viewManagerModel.setActiveView(signupView.viewName);
+        viewManagerModel.firePropertyChanged();
+
+        app.pack();
+        JPanel buttons = (JPanel) signupView.getComponent(5);
+        JButton signupButton = (JButton) buttons.getComponent(0);
+
+        SignupState signupState = signupViewModel.getState();
+        signupState.setUsername("bob");
+
+        signupButton.doClick();
+        assertEquals(viewManagerModel.getActiveView(), "logged in");
+        assertEquals(signupViewModel.getState().getUsername(), "");
+        assertEquals(signupViewModel.getState().getPassword(), "");
+        assertEquals(signupViewModel.getState().getRepeatPassword(), "");
+        assertEquals(loggedInViewModel.getState().getUsername(), "bob");
+
+    }
+
+
+    @Test
+    public void testOpenLandingView() {
+
+        SignupViewModel signupViewModel = new SignupViewModel();
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+
+        SignupInputBoundary sib = null;
+        SignupController signupController = new SignupController(sib);
+        SignupView signupView = new SignupView(signupController, signupViewModel, viewManagerModel);
+
+        LandingView landingView = new LandingView(signupViewModel, new LoginViewModel(), viewManagerModel);
+
+        JFrame app = new JFrame();
+        CardLayout cardLayout = new CardLayout();
+        JPanel views = new JPanel(cardLayout);
+        app.add(views);
+
+        new ViewManager(views, cardLayout, viewManagerModel);
+
+        views.add(signupView, signupView.viewName);
+        views.add(landingView, landingView.viewName);
+
+        viewManagerModel.setActiveView(signupView.viewName);
+        viewManagerModel.firePropertyChanged();
+
+        app.pack();
+
+        JPanel buttons = (JPanel) signupView.getComponent(5);
+        JButton cancelButton = (JButton) buttons.getComponent(2);
+
+        cancelButton.doClick();
+        assertEquals(viewManagerModel.getActiveView(), "landing page");
+        assertEquals(signupViewModel.getState().getUsername(), "");
+        assertEquals(signupViewModel.getState().getPassword(), "");
+        assertEquals(signupViewModel.getState().getRepeatPassword(), "");
+
+    }
+}
