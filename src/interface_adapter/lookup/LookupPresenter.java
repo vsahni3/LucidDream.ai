e8945be9d9a22c27d785bd6308af597275ce65a0
Varun Sahni <varunsahni3504@gmail.com>
2023-12-02 18:41:43 -0500

    add lookup controller

--- src/interface_adapter/lookup/LookupPresenter.java
+++ src/interface_adapter/lookup/LookupPresenter.java
@@ -1,2 +1,9 @@
-package interface_adapter.lookup;public class LookupPresenter {
+package interface_adapter.lookup;
+
+import use_case.lookup.LookupOutputBoundary;
+import use_case.lookup.LookupOutputData;
+
+public class LookupPresenter implements LookupOutputBoundary {
+    @Override
+    public void prepareSuccessView(LookupOutputData lookupOutputData){}
 }
