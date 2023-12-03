16471e2c27f4ef73f376aff9b9b04c61b294ddb5
Varun Sahni <varunsahni3504@gmail.com>
2023-12-03 17:42:44 -0500

    more test coverage

--- test/use_case/lookup/LookupInteractorTest.java
+++ test/use_case/lookup/LookupInteractorTest.java
@@ -20,13 +20,38 @@ class LookupInteractorTest {
             @Override
             public void prepareSuccessView(LookupOutputData lookupOutputData) {
                 String definition = lookupOutputData.getDefinition();
+                String word = lookupOutputData.getWord();
                 assertNotNull(definition);
+                assertNotNull(word);
 
             }
 
             @Override
             public void prepareFailView(String errorMessage) {
                 fail("Use case failure is unexpected.");
+
+            }
+
+        };
+
+        LookupInputBoundary interactor = new LookupInteractor(lookupPresenter);
+        interactor.execute(inputData);
+    }
+
+    @Test
+    void FailTest() {
+        LookupInputData inputData = new LookupInputData(null);
+
+        LookupOutputBoundary lookupPresenter = new LookupOutputBoundary() {
+            @Override
+            public void prepareSuccessView(LookupOutputData lookupOutputData) {
+                fail("Use case success is unexpected.");
+
+            }
+
+            @Override
+            public void prepareFailView(String errorMessage) {
+                assertNotNull(errorMessage);
 
             }
 
